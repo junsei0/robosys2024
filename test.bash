bced9fdc3df4f95c628efa352d76e317647fd9fe
Junsei Iimori <moririn638@gmail.com>
2024-11-27 22:11:38 +0900

    test

--- test.bash
+++ test.bash
@@ -4,21 +4,54 @@
 
 
 ng () {
-	echo ${1}行目が違うだろ違うだろ!!
+	echo NG at line ${1}
 	res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$(echo sin 30 | ./kadai)
+[ "${out}" = 0.50 ] || ng "$LINENO"
+
+out=$(echo cos 30 | ./kadai)
+[ "${out}" = 0.87 ] || ng "$LINENO"
+
+out=$(echo tan 30 | ./kadai)
+[ "${out}" = 0.58 ] || ng "$LINENO"
+
+out=$(echo sin 60 | ./kadai)
+[ "${out}" = 0.87 ] || ng "$LINENO"
+
+out=$(echo cos 60 | ./kadai)
+[ "${out}" = 0.50 ] || ng "$LINENO"
+
+out=$(echo tan 60 | ./kadai)
+[ "${out}" = 1.73 ] || ng "$LINENO"
+
+out=$(echo sin 0 | ./kadai)
+[ "${out}" = 0.00 ] || ng "$LINENO"
+
+out=$(echo cos 0 | ./kadai)
+[ "${out}" = 1.00 ] || ng "$LINENO"
+
+out=$(echo tan 0 | ./kadai)
+[ "${out}" = 0.00 ] || ng "$LINENO"
+
+out=$(echo sin 90 | ./kadai)
+[ "${out}" = 1.00 ] || ng "$LINENO"
+
+out=$(echo cos 90 | ./kadai)
+[ "${out}" = 0.00 ] || ng "$LINENO"
+
+out=$(echo tan 90 | ./kadai)
+[ "${out}" = "Error: tan90は定義できない" ] || ng "$LINENO"
 
 ###変な入力###
-out=$(echo あ | ./plus)
+out=$(echo あ | ./kadai)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
-out=$(echo | ./plus)
+out=$(echo | ./kadai)
 [ "$?" = 1 ]      || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
